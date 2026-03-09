95f98e9d80b83af58ba9d1f1b4de4859b6f512ef
brad-go <dhjk35@naver.com>
2026-03-09 22:44:24 +0900

    solve(cpp): 백준 25083 문제 풀이

---
+++ algorithms/boj/25083/README.md
@@ -0,0 +1,21 @@
+# 25083. 새싹
+
+## Problem
+
+아래 예제와 같이 새싹을 출력하시오.
+
+## Input
+
+입력은 없다.
+
+## Output
+
+새싹을 출력한다.
+
+## Classification
+
+- [구현](../README.md/#implemenation)
+
+## Level
+
+- [Bronze 5](../../boj/README.md/#bronze-5)
---
+++ algorithms/boj/25083/cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  std::string buffer;
+
+  buffer += "         ,r'\"7\n";
+  buffer += "r`-_   ,'  ,/\n";
+  buffer += " \\. \". L_r'\n";
+  buffer += "   `~\\/\n";
+  buffer += "      |\n";
+  buffer += "      |\n";
+
+  std::cout << buffer;
+}
